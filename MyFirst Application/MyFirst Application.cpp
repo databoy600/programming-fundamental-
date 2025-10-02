19cc2c803a8bd4cad93a206ea3518f84a76794b3
databoy600 <joshua.its4u@icloud.com>
2025-10-02 10:37:25 +0100

    start of cod tutoral

---
+++ MyFirst Application/MyFirst Application.cpp
@@ -0,0 +1,24 @@
+// MyFirst Application.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int number = 0; 
+
+int main()
+{
+    cout << "Hello there!\n";
+    cout << "Name: Joshua Kimbley!\n";
+    cout << "Course: Game Design and Programming!\n";
+    cout << "Student ID: 25017713\n";
+
+    cout << "*\n";
+    cout << "**\n";
+    cout << "*  *\n";
+    cout << "****\n";
+    cout << "Hello...\t\t\t\t there!\n";
+
+    cout << "2+2" << "=" << "4";
+}
+
+
